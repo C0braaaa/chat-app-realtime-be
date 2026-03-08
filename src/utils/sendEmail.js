d1373cfd66253666d3403cf213de79a79c4f2991
C0braaaa <cobragaming0fo@gmail.com>
2026-03-08 19:43:29 +0700

    Refactor sendEmail transporter configuration for improved clarity

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,9 +5,15 @@ export const sendEmail = async (options) => {
   // Cấu hình transporter (Dùng Gmail)
   const transporter = nodemailer.createTransport({
     service: "Gmail",
+    host: env.SMTP_HOST,
+    port: env.SMTP_PORT,
+    secure: false,
     auth: {
       user: env.SMTP_USER,
       pass: env.SMTP_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
