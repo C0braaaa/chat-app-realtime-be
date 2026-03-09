41d52608187992e4b55a7991a012fe7df510bcc5
C0braaaa <cobragaming0fo@gmail.com>
2026-03-09 19:35:06 +0700

    refactor sendEmail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,28 +1,30 @@
-import nodemailer from "nodemailer";
-import { env } from "#src/config/environment.js";
+import axios from "axios";
+import { env } from "~/config/environment";
 
-export const sendEmail = async (options) => {
-  // Cấu hình transporter (Dùng Gmail)
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: env.SMTP_HOST,
-    port: env.SMTP_PORT,
-    secure: false,
-    auth: {
-      user: env.SMTP_USER,
-      pass: env.SMTP_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  const mailOptions = {
-    from: '"C Chat App" <' + env.SMTP_USER + ">",
-    to: options.email,
+const sendEmail = async (options) => {
+  const emailData = {
+    sender: { name: "Cflix Support", email: env.SMTP_USER },
+    to: [{ email: options.email }],
     subject: options.subject,
-    text: options.message,
+    htmlContent: options.html,
   };
 
-  await transporter.sendMail(mailOptions);
+  try {
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      emailData,
+      {
+        headers: {
+          accept: "application/json",
+          "api-key": env.SMTP_PASS,
+          "content-type": "application/json",
+        },
+      },
+    );
+    console.log("Email gửi thành công qua API:", response.data.messageId);
+  } catch (error) {
+    console.error("Lỗi API Brevo:", error.response?.data || error.message);
+  }
 };
+
+export default sendEmail;
